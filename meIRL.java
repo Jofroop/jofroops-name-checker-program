8980633e66a2ed8b52423ac3d96de5713556840f
Jeffrey Daniel <47395487+Jofroop@users.noreply.github.com>
2020-12-23 20:02:47 +0000

    added loop

--- meIRL.java
+++ meIRL.java
@@ -2,29 +2,45 @@ import java.util.Scanner;
 
 public class meIRL {
     static void EnterName(){
+        //set variables
+        String validinput = "false";
+
         //take input
         Scanner nameInput = new Scanner(System.in);
         System.out.println("Please enter your name.");
         String name = nameInput.nextLine();
 
-        //print the input along with an additional message
-        System.out.println("Your name is " + name + ".");
+        while (validinput == "false"){
+            //print the input along with an additional message
+            System.out.println("Your name is " + name + ".");
 
-        //asking if the name is correct with a y/n prompt
-        //this will ask you to re-enter if it is not y or n
-        System.out.println("Is this correct?");
-        Scanner isNameCorrect = new Scanner(System.in);
-        String isCorrectVar = isNameCorrect.nextLine();
+            //asking if the name is correct with a y/n prompt
+            //this will ask you to re-enter if it is not y or n
+            System.out.println("Is this correct? (Y/N)");
+            Scanner isNameCorrect = new Scanner(System.in);
+            String isCorrectVar = isNameCorrect.nextLine();
 
-        switch (isCorrectVar.toLowerCase()) {
-            case "y":
-                System.out.println("Good. The program will now terminate.");
-                break;
+            //loop for testing if the input is good
+            //if input is bad then it loops back and asks you to enter again
+            //a bad input would be "h" or "k" or "beans"
+            switch (isCorrectVar.toLowerCase()) {
+                case "y":
+                    System.out.println("Good. The program will now terminate.");
+                    validinput = "true";
+                    break;
 
-            case "n":
-                System.out.println("You will now be asked to re-enter your name.");
-                EnterName();
-                break;
+                case "n":
+                    System.out.println("You will now be asked to re-enter your name.");
+                    validinput = "true";
+                    EnterName();
+                    break;
+
+                default:
+                    System.out.println("Invalid input.");
+                    validinput = "false";
+                    break;
+        }
+
 
         }
     }
